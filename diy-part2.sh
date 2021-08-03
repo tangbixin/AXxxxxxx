23ad3a5eecef6872ae75e9db926bc812ea58a4da
admin6750217 <58500194+admin6750217@users.noreply.github.com>
2021-08-03 10:20:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
- sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+ sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
 
 # themes添加
 #git clone https://github.com/Leo-Jo-My/luci-theme-Butterfly package/luci-theme-Butterfly
